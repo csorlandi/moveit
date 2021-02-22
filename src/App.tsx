e292f8a4d9df2f8b923bac28375b4df93e2ea626
Claudio Orlandi <cso.junior1996@gmail.com>
2021-02-22 17:19:25 -0300

    Create Experience Bar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import React from 'react';
+
+import { ExperienceBar } from './components/ExperienceBar';
+
 import './styles/global.css';
 
 function App() {
   return (
-    <h1>Hello NLW</h1>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
