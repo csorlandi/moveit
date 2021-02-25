13e8549b396aff5696d595e7325c5dfe247792d2
Claudio Orlandi <cso.junior1996@gmail.com>
2021-02-24 23:14:26 -0300

    Create Completed Challanges Component

---
+++ src/components/CompletedChallanges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallanges.module.css';
+
+export function CompletedChallanges() {
+  return (
+    <div className={styles.completedChallangesContainer}>
+      <span>Desafios Completos</span>
+      <span>5</span>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CompletedChallanges } from "../components/CompletedChallanges";
 
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
@@ -13,6 +14,7 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallanges />
         </div>
         <div>
 
